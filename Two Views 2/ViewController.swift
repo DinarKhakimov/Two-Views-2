a03b26ad28543fa20a561f6db2a359cedbbf39a7
Johnny Boshechka <in_order@icloud.com>
2021-12-09 19:23:52 +0500

    Initial Commit

---
+++ Two Views 2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Two Views 2
+//
+//  Created by Johnny Boshechka on 12/9/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
